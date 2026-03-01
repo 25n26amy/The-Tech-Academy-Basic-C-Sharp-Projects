4a96596fff3a87d4bc431738052d373c080287a1
25n26amy <partiksandhu04@gmail.com>
2026-03-01 15:17:03 -0700

    Added files
    Branching assignment submission for .NET PART 1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.WriteLine("Please enter the package weight:");
+        decimal weight = Convert.ToDecimal(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        Console.WriteLine("Please enter the package width:");
+        decimal width = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package height:");
+        decimal height = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package length:");
+        decimal length = Convert.ToDecimal(Console.ReadLine());
+
+        decimal totalDimensions = width + height + length;
+
+        if (totalDimensions > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return;
+        }
+
+        decimal volume = width * height * length;
+        decimal totalCost = volume * weight;
+        decimal quote = totalCost / 100;
+
+        Console.WriteLine("Your estimated total for shipping this package is: $" + quote.ToString("F2"));
+        Console.WriteLine("Thank you!");
+    }
+}
